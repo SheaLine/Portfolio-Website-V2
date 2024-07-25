cfa3a10f65b0cf550d32b27d7fbb80013f94fc64
SheaLine <sline@ucsc.edu>
2024-07-24 18:01:16 -0700

    updated paths for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import "./App.css";
 
 export default function App() {
   return (
-    <Scene
-      VideoSrc={`/Portfolio-Website-V2/public/Blender-Camp-Scene.mp4`}
-      AudioSrc={`/Portfolio-Website-V2/public/CampAudio.mp3`}
-    >
+    <Scene VideoSrc={`./Blender-Camp-Scene.mp4`} AudioSrc={`./CampAudio.mp3`}>
       {/* Add interactive objects here */}
       <div className="interactive-object" style={{ top: "20%", left: "40%" }}>
         <button onClick={() => alert("Clicked!")}>Click Me</button>
