f1b6f0c9554369a6b9e6e91c94276d0a07884b87
SheaLine <sline@ucsc.edu>
2024-08-27 13:51:21 -0700

    put back button into own component

--- src/components/BackButton/BackButton.jsx
+++ src/components/BackButton/BackButton.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { IoArrowBackCircle } from "react-icons/io5";
+import "./BackButton.css";
 
 function BackButton({ handleFadeOut }) {
   const navigate = useNavigate();
