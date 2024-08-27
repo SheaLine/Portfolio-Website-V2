6b0cf272bd93160c9b0126d4f04f265158d6468b
SheaLine <sline@ucsc.edu>
2024-08-27 12:23:31 -0700

    higher order component to apply fade effect when changing pages

---
+++ src/components/Fade.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import HoverBox from "./HoverBox";
+
+function withFadeEffect(OriginalComponent, initialFadeClass = "fade-enter") {
+  function NewComponent(props) {
+    const [fadeClass, setFadeClass] = useState(initialFadeClass);
+
+    useEffect(() => {
+      setTimeout(() => {
+        setFadeClass("fade-enter-active");
+      }, 30); //fade-in with slight delay to ensure transition is triggered
+    }, []);
+
+    const handleFadeOut = (callback) => {
+      setFadeClass("fade-exit-active");
+      setTimeout(callback, 500); // Ensure this matches the duration of your fade-out CSS transition
+    };
+
+    return (
+      <div className={fadeClass}>
+        <OriginalComponent {...props} handleFadeOut={handleFadeOut} />
+      </div>
+    );
+  }
+  return NewComponent;
+}
+
+export default withFadeEffect;
